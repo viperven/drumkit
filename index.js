872b082ac2d54b50de9880918e48fd7c7e2edaad
Rupesh Jha <127649834+viperven@users.noreply.github.com>
2023-04-24 00:31:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+
+// for mouse click event listner play sound when any key image is cliked
+
+for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        let buttonPressed = this.innerHTML;
+
+        switch (buttonPressed) {
+            case "w":
+                let soudW = new Audio("sounds/crash.mp3");
+                soudW.play();
+                break;
+            case "a":
+                let soudA = new Audio("sounds/kick-bass.mp3");
+                soudA.play();
+                break;
+            case "s":
+                let soudS = new Audio("sounds/snare.mp3");
+                soudS.play();
+                break;
+            case "d":
+                let soudD = new Audio("sounds/tom-1.mp3");
+                soudD.play();
+                break;
+            case "j":
+                let soudJ = new Audio("sounds/tom-2.mp3");
+                soudJ.play();
+                break;
+            case "k":
+                let soudK = new Audio("sounds/tom-3.mp3");
+                soudK.play();
+                break;
+            case "l":
+                let soudL = new Audio("sounds/tom-4.mp3");
+                soudL.play();
+                break;
+
+            default: console.log(buttonPressed);
+        }
+    });
+}
+
+// for keyboard event listener and play sound when any key is pressed
+
+document.addEventListener("keydown",
+    function (event) {
+        switch (event.key) {
+            case "w":
+                let soudW = new Audio("sounds/crash.mp3");
+                soudW.play();
+                break;
+            case "a":
+                let soudA = new Audio("sounds/kick-bass.mp3");
+                soudA.play();
+                break;
+            case "s":
+                let soudS = new Audio("sounds/snare.mp3");
+                soudS.play();
+                break;
+            case "d":
+                let soudD = new Audio("sounds/tom-1.mp3");
+                soudD.play();
+                break;
+            case "j":
+                let soudJ = new Audio("sounds/tom-2.mp3");
+                soudJ.play();
+                break;
+            case "k":
+                let soudK = new Audio("sounds/tom-3.mp3");
+                soudK.play();
+                break;
+            case "l":
+                let soudL = new Audio("sounds/tom-4.mp3");
+                soudL.play();
+                break;
+
+            default: console.log(buttonPressed);
+        }
+    });
+
