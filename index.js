655e3c2e718a00649b96aaec22443ed1645e0d3d
Rupesh Jha <127649834+viperven@users.noreply.github.com>
2023-04-30 22:23:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,34 +5,43 @@ for (i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         let buttonPressed = this.innerHTML;
 
+        //    animation(buttonPressed);
+
         switch (buttonPressed) {
             case "w":
                 let soudW = new Audio("sounds/crash.mp3");
                 soudW.play();
+                animation(".w");
                 break;
             case "a":
                 let soudA = new Audio("sounds/kick-bass.mp3");
                 soudA.play();
+                animation(".a");
                 break;
             case "s":
                 let soudS = new Audio("sounds/snare.mp3");
                 soudS.play();
+                animation(".s")
                 break;
             case "d":
                 let soudD = new Audio("sounds/tom-1.mp3");
                 soudD.play();
+                animation(".d");
                 break;
             case "j":
                 let soudJ = new Audio("sounds/tom-2.mp3");
                 soudJ.play();
+                animation(".j");
                 break;
             case "k":
                 let soudK = new Audio("sounds/tom-3.mp3");
                 soudK.play();
+                animation(".k");
                 break;
             case "l":
                 let soudL = new Audio("sounds/tom-4.mp3");
                 soudL.play();
+                animation(".l");
                 break;
 
             default: console.log(buttonPressed);
@@ -48,33 +57,52 @@ document.addEventListener("keydown",
             case "w":
                 let soudW = new Audio("sounds/crash.mp3");
                 soudW.play();
+                animation(".w");
                 break;
             case "a":
                 let soudA = new Audio("sounds/kick-bass.mp3");
                 soudA.play();
+                animation(".a");
                 break;
             case "s":
                 let soudS = new Audio("sounds/snare.mp3");
                 soudS.play();
+                animation(".s");
                 break;
             case "d":
                 let soudD = new Audio("sounds/tom-1.mp3");
                 soudD.play();
+                animation(".d");
                 break;
             case "j":
                 let soudJ = new Audio("sounds/tom-2.mp3");
                 soudJ.play();
+                animation(".j");
                 break;
             case "k":
                 let soudK = new Audio("sounds/tom-3.mp3");
                 soudK.play();
+                animation(".k");
                 break;
             case "l":
                 let soudL = new Audio("sounds/tom-4.mp3");
                 soudL.play();
+                animation(".l");
                 break;
 
             default: console.log(buttonPressed);
         }
     });
+
+
+// for animation clik and key board event
+
+function animation(key) {
+    document.querySelector(key).classList.add("pressed");
+
+    setTimeout(function () {
+        document.querySelector(key).classList.remove("pressed");  //to remove class
+    }, 100);
+
+}
 
